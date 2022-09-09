a3500b746701ef3d8cf660cb4d70186b4609166f
nickbao1996 <yeyi1996@qq.com>
2022-09-09 18:15:29 +0800

    初始化

---
+++ web_api_learn/src/main/java/com/example/web_api_learn/WebApiLearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.web_api_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApiLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApiLearnApplication.class, args);
+    }
+
+}
